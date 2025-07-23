daa52f08db0e2e2e99c118c3110ca858a4aee5f8
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-07-23 11:28:44 -0300

    header styling and fontawesome install

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBed, faPlane, faCar, faTaxi } from '@fortawesome/free-solid-svg-icons'
+import './header.css'
+
+export const Header = () => {
+    return (
+        <div className="header">
+            <div className="headerContainer">
+                <div className="headerList">
+                    <div className="headerListItem active">
+                        <FontAwesomeIcon icon={faBed} />
+                        <span>Stays</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faPlane} />
+                        <span>Flights</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faCar} />
+                        <span>Car rentals</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faTaxi} />
+                        <span>Taxi</span>
+                    </div>
+                </div>
+                <div className="headerTitle">A lifetime of discounts? It's Genius.</div>
+                <p className="headerDesc">
+                    Get rewarded for your travels - unlock instant savings of 10% or more with a free PenkeBooking account
+                </p>
+                <button className="headerBtn">Sign in / Register</button>
+                
+
+            </div>
+        </div>
+    )
+}
