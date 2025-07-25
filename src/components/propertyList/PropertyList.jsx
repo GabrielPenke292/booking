8e19abef2be5c719a5a5d113dd8aca54dc9e64bc
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-07-25 09:45:48 -0300

    creating propertyList component

---
+++ src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './propertyList.css'
+
+export const PropertyList = () => {
+  return (
+    <div>
+      <div className="pList">
+        <div className="pListItem">
+          <img src="https://cf.bstatic.com/xdata/images/hotel/square600/587051339.webp?k=7d0aacae79dc30776172b140027d30a7feb57ca7bc853f1218af50daca9c762a&o=" alt="" className="pListImg" />
+          <div className="pListTitles">
+            <h1>Hotels</h1>
+            <h2>233 hotels</h2>
+          </div>
+        </div>
+        <div className="pListItem">
+          <img src="https://cf.bstatic.com/xdata/images/hotel/square600/587051339.webp?k=7d0aacae79dc30776172b140027d30a7feb57ca7bc853f1218af50daca9c762a&o=" alt="" className="pListImg" />
+          <div className="pListTitles">
+            <h1>Apartments</h1>
+            <h2>233 apartments</h2>
+          </div>
+        </div>
+        <div className="pListItem">
+          <img src="https://cf.bstatic.com/xdata/images/hotel/square600/587051339.webp?k=7d0aacae79dc30776172b140027d30a7feb57ca7bc853f1218af50daca9c762a&o=" alt="" className="pListImg" />
+          <div className="pListTitles">
+            <h1>Resorts</h1>
+            <h2>233 resorts</h2>
+          </div>
+        </div>
+        <div className="pListItem">
+          <img src="https://cf.bstatic.com/xdata/images/hotel/square600/587051339.webp?k=7d0aacae79dc30776172b140027d30a7feb57ca7bc853f1218af50daca9c762a&o=" alt="" className="pListImg" />
+          <div className="pListTitles">
+            <h1>Villas</h1>
+            <h2>233 villas</h2>
+          </div>
+        </div>
+        <div className="pListItem"> 
+          <img src="https://cf.bstatic.com/xdata/images/hotel/square600/587051339.webp?k=7d0aacae79dc30776172b140027d30a7feb57ca7bc853f1218af50daca9c762a&o=" alt="" className="pListImg" />
+          <div className="pListTitles"> 
+            <h1>Cabins</h1>
+            <h2>233 cabins</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import './home.css'
 import { Navbar } from '../../components/navbar/Navbar'
 import { Header } from '../../components/header/Header'
 import { Featured } from '../featured/Featured'
+import { PropertyList } from '../../components/propertyList/PropertyList'
 
 export const Home = () => {
   return (
@@ -12,6 +13,8 @@ export const Home = () => {
 
       <div className="homeContainer">
         <Featured />
+        <div className="homeTitle">Browse by property type</div>
+        <PropertyList />
       </div>
     </div>
   )
