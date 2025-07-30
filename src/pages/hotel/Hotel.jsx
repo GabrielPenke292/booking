fc360676eff3246be308ad85f1ec43d9e754378c
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-07-29 21:35:00 -0300

    component single hotel data

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,7 +1,68 @@
 import React from 'react'
+import './hotel.css'
+import { Navbar } from '../../components/navbar/Navbar'
+import { Header } from '../../components/header/Header'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 
 export const Hotel = () => {
+  const photos = [
+    {
+      src: "https://r-xx.bstatic.com/xdata/images/hotel/263x210/595548591.jpeg?k=01741bc3aef1a5233dd33794dda397083092c0215b153915f27ea489468e57a2&o=",
+    },
+    {
+      src: "https://r-xx.bstatic.com/xdata/images/hotel/263x210/595548591.jpeg?k=01741bc3aef1a5233dd33794dda397083092c0215b153915f27ea489468e57a2&o=",
+    },
+    {
+      src: "https://r-xx.bstatic.com/xdata/images/hotel/263x210/595548591.jpeg?k=01741bc3aef1a5233dd33794dda397083092c0215b153915f27ea489468e57a2&o=",
+    },
+    {
+      src: "https://r-xx.bstatic.com/xdata/images/hotel/263x210/595548591.jpeg?k=01741bc3aef1a5233dd33794dda397083092c0215b153915f27ea489468e57a2&o=",
+    },
+    {
+      src: "https://r-xx.bstatic.com/xdata/images/hotel/263x210/595548591.jpeg?k=01741bc3aef1a5233dd33794dda397083092c0215b153915f27ea489468e57a2&o=",
+    },
+  ];
   return (
-    <div>Hotel</div>
+    <div>
+      <Navbar />
+      <Header type="list" />
+      <div className="hotelContainer">
+        <div className="hotelWrapper">
+          <h1 className="hotelTitle">Grand Hotel</h1>
+          <div className="hotelAddress">
+            <FontAwesomeIcon icon={faLocationDot} />
+            <span>Elton St, 125 New York</span>
+          </div>
+          <span className="hotelDistance">Excellent location - 500m from center</span>
+          <span className="hotelPriceHighlight">Book a stay over $114 at this property and get a free airport taxi</span>
+          <div className="hotelImages">
+            {photos.map((photo, index)=>(
+              <div className="hotelImageWrapper" key={index}>
+                <img src={photo.src} alt="" className="hotelImage" />
+              </div>
+            ))}
+          </div> {/* hotelImages */}
+          <div className="hotelDetails">
+            <div className="hotelDetailsText">
+              <h1 className="hotelTitle">Stay in the heart of Krakow</h1>
+              <p className="hotelDesc">
+                Located in the real heart of Krakow, this property has an excellent location score 9.8!
+              </p>
+            </div>
+            <div className="hotelDetailsPrice">
+              <h1>Perfect for a 1-night stay!</h1>
+              <span>
+                Located in the real heart of Krakow, this property has an excellent location score 9.8!
+              </span>
+              <h2>
+                <b>$112</b> (1 night)
+              </h2>
+              <button>Reserve or Book Now!</button>
+            </div>
+          </div>{/* hotelDetails */}
+        </div>
+      </div>
+    </div>
   )
 }
