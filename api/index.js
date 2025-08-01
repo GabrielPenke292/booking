9fdaa75335b028061f56e047550b70fad4ce5574
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-08-01 09:35:22 -0300

    creating middlewares

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose"; 
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
+import roomsRoute from "./routes/rooms.js";
+
 const app = express();
 dotenv.config();
 
@@ -20,6 +25,12 @@ mongoose.connection.on("disconnected", () => {
 mongoose.connection.on("connected", () => {
     console.log("mongoDB connected!");
 })
+
+// middlewares
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/users", usersRoute);
+app.use("/api/v1/hotels", hotelsRoute);
+app.use("/api/v1/rooms", roomsRoute);
 
 app.listen(8800, () => {
     connect();
