02461033c7c749044d721a695ac913d23af9002c
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-08-02 15:44:00 -0300

    Update hotel

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -15,7 +15,14 @@ router.post("/", async (req, res) => {
 })
 
 // UPDATE
-
+router.put("/:id", async (req, res) => {
+    try{
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true});
+        res.status(200).json(updatedHotel);
+    }catch(err){
+        res.status(500).json({message: "Internal server error"})
+    }
+})
 // DELETE
 
 // GET
