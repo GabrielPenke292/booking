dd4dd04ddd0b0a7f5122311f16e52a7d8582b5a7
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-08-02 10:55:36 -0300

    POST to create new hotel

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,11 +1,19 @@
 import express from "express";
+import Hotel from "../models/Hotel.js";
 
 const router = express.Router();
 
 // CREATE
-router.post("/", (req, res) => {
-    
+router.post("/", async (req, res) => {
+    try{
+        const newHotel = new Hotel(req.body);
+        await newHotel.save();
+        res.status(201).json(newHotel);
+    }catch(err){
+        res.status(500).json({message: "Internal server error"})
+    }
 })
+
 // UPDATE
 
 // DELETE
