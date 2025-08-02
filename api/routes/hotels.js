009843029412be0232cdb5658c06bbe7d03c3031
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-08-02 15:58:40 -0300

    Get all Hotels

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -45,5 +45,13 @@ router.get("/:id", async (req, res) => {
 })
 
 //GET ALL
+router.get("/", async (req, res) => {
+    try{
+        const hotels = await Hotel.find();
+        res.status(200).json(hotels);
+    }catch(err){
+        res.status(500).json({message: "Internal server error"})
+    }
+})
 
 export default router;
