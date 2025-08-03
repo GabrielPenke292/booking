e8e7f4bd57947896b9b31cd2d69e4d713884fde3
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-08-03 10:13:46 -0300

    Using JWT to authentication

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 import { createError } from "../utils/error.js";
 
 export const register = async (req, res, next) => {
@@ -24,10 +25,9 @@ export const login = async (req, res, next) => {
         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
         if(!isPasswordCorrect) return next(createError(400, "Wrong password or username"));
         const { password, isAdmin, ...otherDetails } = user._doc;
-        res.status(200).json(otherDetails);
-        // const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT);
-        // const {password, ...otherDetails} = user._doc;
-        // res.cookie("access_token", token, {httpOnly: true}).status(200).json(otherDetails);
+
+        const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT);
+        res.cookie("access_token", token, {httpOnly: true}).status(200).json(otherDetails);
     }catch(err){
         next(err);
     }
