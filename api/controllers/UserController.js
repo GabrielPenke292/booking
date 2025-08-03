e295902ff398a9b3d3e75036e35b83ad7ba43ce5
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-08-02 21:51:54 -0300

    Instaling and using bcrypt for security

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,8 +1,15 @@
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 
 export const createUser = async (req, res, next) => {
     try {
-        const user = new User(req.body);
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(req.body.password, salt);
+        const user = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hash,
+        });
         await user.save();
         res.status(201).json(user);
     } catch (err) {
