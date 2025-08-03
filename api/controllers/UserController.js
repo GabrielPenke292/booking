77890a8b05996aae8e29e5e78632c6b153873e55
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-08-03 10:24:40 -0300

    Creating user controller

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,17 +1,46 @@
 import User from "../models/User.js";
-import bcrypt from "bcryptjs";
 
 export const createUser = async (req, res, next) => {
     try {
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(req.body.password, salt);
-        const user = new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: hash,
-        });
-        await user.save();
-        res.status(201).json(user);
+        const newUser = new User(req.body);
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch (err) {
+        next(err);
+    }
+}
+
+export const updateUser = async (req, res, next) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        next(err);
+    }
+}
+
+export const deleteUser = async (req, res, next) => {
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted");
+    } catch (err) {
+        next(err);
+    }
+}
+
+export const getUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(200).json(user);
+    } catch (err) {
+        next(err);
+    }
+}
+
+export const getUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
     } catch (err) {
         next(err);
     }
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,21 @@
 import express from "express";
-import { createUser } from "../controllers/UserController.js";
+import { createUser, updateUser, deleteUser, getUser, getUsers } from "../controllers/UserController.js";
 
 const router = express.Router();
 
 // CREATE
 router.post("/", createUser);
+
+// UPDATE
+router.put("/:id", updateUser);
+
+// DELETE
+router.delete("/:id", deleteUser);
+
+// GET
+router.get("/:id", getUser);
+
+// GET ALL
+router.get("/", getUsers);
 
 export default router;
