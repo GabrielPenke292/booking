6290fba88b9cf43293add06ac4dd7a2c94dc1aba
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-08-03 23:26:56 -0300

    verify user function

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,18 @@
 import express from "express";
 import { createUser, updateUser, deleteUser, getUser, getUsers } from "../controllers/UserController.js";
+import { verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+// verifyToken
+router.get("/checkauthentication", verifyToken, (req, res, next) => {
+    res.send("Hello user, you are logged in");
+});
+
+// verifyUser
+router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+    res.send("Hello user, you are logged in and you can delete your account");
+});
 
 // CREATE
 router.post("/", createUser);
