4360785a6ef02d04bad889d0f18dcb11a7833523
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-08-05 21:22:16 -0300

    creating Room Controller and model

---
+++ api/controllers/RoomController.js
@@ -0,0 +1,7 @@
+import Room from "../models/Room.js";
+import Hotel from "../models/Hotel.js";
+import {createError} from "../utils/error.js";
+
+export const createRoom = async (req, res, next) => {
+
+}
