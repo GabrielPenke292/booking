d5145c9e3c73c130bf7852f1f1dbc18966095891
Gabriel Souza de Aguiar Penke <gabriel.penke@gruposn.com.br>
2025-08-05 21:10:45 -0300

    Add middleware to check permission in users and hotels routes

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,32 +1,37 @@
 import express from "express";
 import { createUser, updateUser, deleteUser, getUser, getUsers } from "../controllers/UserController.js";
-import { verifyToken, verifyUser } from "../utils/verifyToken.js";
+import { verifyToken, verifyUser, verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-// verifyToken
-router.get("/checkauthentication", verifyToken, (req, res, next) => {
-    res.send("Hello user, you are logged in");
-});
+// // verifyToken
+// router.get("/checkauthentication", verifyToken, (req, res, next) => {
+//     res.send("Hello user, you are logged in");
+// });
 
-// verifyUser
-router.get("/checkuser/:id", verifyUser, (req, res, next) => {
-    res.send("Hello user, you are logged in and you can delete your account");
-});
+// // verifyUser
+// router.get("/checkuser/:id", verifyUser, (req, res, next) => {
+//     res.send("Hello user, you are logged in and you can delete your account");
+// });
+
+// // verifyAdmin
+// router.get("/checkadmin/:id", verifyAdmin, (req, res, next) => {
+//     res.send("Hello admin, you are logged in and you can delete all accounts");
+// });
 
 // CREATE
 router.post("/", createUser);
 
 // UPDATE
-router.put("/:id", updateUser);
+router.put("/:id", verifyUser, updateUser);
 
 // DELETE
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
 
 // GET
-router.get("/:id", getUser);
+router.get("/:id", verifyUser, getUser);
 
 // GET ALL
-router.get("/", getUsers);
+router.get("/", verifyAdmin, getUsers);
 
 export default router;
